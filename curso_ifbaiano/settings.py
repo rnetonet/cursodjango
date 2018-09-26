ea06046eaa051e190addaff0bc651bae93ca5afc
rnetonet <rneto@rneto.net>
2018-09-26 14:48:35 -0300

    Remove pyc; .kepp public media

--- curso_ifbaiano/settings.py
+++ curso_ifbaiano/settings.py
@@ -118,13 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_URL = '/public/'
 MEDIA_URL = '/public/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "public")
 ]
 
